d43f8cd55998aecdd137cd39e1410e5c1436cf78
Dario Dündar <duen@zhaw.ch>
2015-07-27 16:06:39 +0200

    working with +, - symbols

--- Source/logic.js
+++ Source/logic.js
@@ -492,17 +492,17 @@ function list(){
 //    if($state[$i]!='killed'){
       if($mass[$i]==0){
         if($state[$i]=='new'){
-          $("#to_buy").append("<li data-icon=\"check\" id=\"item"+$id[$i]+"\" onclick=\"remove_n('item"+$id[$i]+"')\"><u>"+$oname[$i]+"</u></li>");
+          $("#to_buy").append("<li data-icon=\"check\" id=\"item"+$id[$i]+"\" onclick=\"remove_n('item"+$id[$i]+"')\">[+] <u>"+$oname[$i]+"</u></li>");
         }else if($state[$i]=='killed'){
-          $("#to_buy").append("<li data-icon=\"check\" id=\"item"+$id[$i]+"\" onclick=\"remove_n('item"+$id[$i]+"')\"><s>"+$oname[$i]+"</s></li>");
+          $("#to_buy").append("<li data-icon=\"check\" id=\"item"+$id[$i]+"\" onclick=\"remove_n('item"+$id[$i]+"')\">[-] <s>"+$oname[$i]+"</s></li>");
         }else{
           $("#to_buy").append("<li data-icon=\"check\" id=\"item"+$id[$i]+"\" onclick=\"remove_n('item"+$id[$i]+"')\">"+$oname[$i]+"</li>");
         }
       }else{
         if($state[$i]=='new'){
-          $("#to_buy").append("<li data-icon=\"check\" id=\"item"+$id[$i]+"\" onclick=\"remove_n('item"+$id[$i]+"')\"><u>"+$oname[$i]+" x "+$mass[$i]+"</u></li>");
+          $("#to_buy").append("<li data-icon=\"check\" id=\"item"+$id[$i]+"\" onclick=\"remove_n('item"+$id[$i]+"')\">[+] <u>"+$oname[$i]+" x "+$mass[$i]+"</u></li>");
         }else if($state[$i]=='killed'){
-          $("#to_buy").append("<li data-icon=\"check\" id=\"item"+$id[$i]+"\" onclick=\"remove_n('item"+$id[$i]+"')\"><s>"+$oname[$i]+" x "+$mass[$i]+"</s></li>");
+          $("#to_buy").append("<li data-icon=\"check\" id=\"item"+$id[$i]+"\" onclick=\"remove_n('item"+$id[$i]+"')\">[-] <s>"+$oname[$i]+" x "+$mass[$i]+"</s></li>");
         }else{
           $("#to_buy").append("<li data-icon=\"check\" id=\"item"+$id[$i]+"\" onclick=\"remove_n('item"+$id[$i]+"')\">"+$oname[$i]+" x "+$mass[$i]+"</li>");
         }
