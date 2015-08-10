b987f3d0fd8e85a2ec62a514ce938cf9e8c7edeb
Dario Dündar <duen@zhaw.ch>
2015-08-10 10:47:38 +0200

    e-mail if new list

--- Source/register.php
+++ Source/register.php
@@ -20,6 +20,14 @@ $nrRow = mysql_num_rows(mysql_query("SELECT lname FROM list where lname='".$lnam
 if($nrRow==0){
     $dbAll=mysql_query("INSERT INTO list( lname, pw ) VALUES ('".$lname."', '".$pw."')");
     
+    $to      = 'dario.duendar@gmail.com';
+    $subject = 'New PIGL-List: '.$lname.'';
+    $message = 'Eine neue Liste wurde erstellt. Listenname: '.$lname.'';
+    $headers = 'From: pigl.noreply@duendar.ch' . "\r\n" .
+        'Reply-To: pigl.noreply@duendar.ch' . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    mail($to, $subject, $message, $headers);
+    
     $dbAll=mysql_query("SELECT lname, pw FROM list where lname='".$lname."'");
     $row = mysql_fetch_array($dbAll);
     if($row['pw']==$pw){
