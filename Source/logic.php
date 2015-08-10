da5b70ff473ad4f73f2c43deea15bbb2d13381be
Dario Dündar <duen@zhaw.ch>
2015-08-10 14:06:15 +0200

    another php correction

--- Source/logic.php
+++ Source/logic.php
@@ -16,90 +16,92 @@ $state = json_decode($strState,true);
 
 
 
-$con = mysql_connect($mysql_host,$mysql_user,$mysql_password);
-if (!$con)
-{
-  die('Could not connect: ' . mysql_error());
-}
-mysql_select_db($mysql_database, $con);
+if($lname!="0" && $lname!=""){
+  $con = mysql_connect($mysql_host,$mysql_user,$mysql_password);
+  if (!$con)
+  {
+    die('Could not connect: ' . mysql_error());
+  }
+  mysql_select_db($mysql_database, $con);
 
 
-$pw=md5($pw);
-$dbAll=mysql_query("SELECT lname, pw FROM list where lname='".$lname."'");
-$i=0;
-$dbOname = array();
-$dbMass = array();
-$row = mysql_fetch_array($dbAll);
-if($row['pw']==$pw){
-  $ok=true;
-  $lname=$row['lname'];
-}
+  $pw=md5($pw);
+  $dbAll=mysql_query("SELECT lname, pw FROM list where lname='".$lname."'");
+  $i=0;
+  $dbOname = array();
+  $dbMass = array();
+  $row = mysql_fetch_array($dbAll);
+  $ok=false;
+  if($row['pw']==$pw){
+    $ok=true;
+    $lname=$row['lname'];
+  }
 
 
 
-if($ok){
-$dbAll=mysql_query("SELECT oname, mass FROM object where lname='".$lname."' ORDER BY oname");
-$i=0;
-$dbOname = array();
-$dbMass = array();
-while($row = mysql_fetch_array($dbAll)){
-  $dbOname[$i]=$row['oname'];
-  $dbMass[$i]=$row['mass'];
-  $i++;
-}
-  
-  
-for($i=0;$i<count($oname);$i++){
-  if($state[$i]!='normal'){
-    $exist=false;
-    for($j=0;$j<count($dbOname);$j++){
-      if($oname[$i]==$dbOname[$j]){
-        $exist=true;
-        if($state[$i]=='new'){
-          $sql="UPDATE object SET mass='".$mass[$i]."' WHERE lname='".$lname."' AND oname='".$oname[$i]."'";
-          mysql_query($sql);
-        }else if($state[$i]=='killed'){
-          $sql="DELETE from object where lname='".$lname."' AND oname='".$oname[$i]."'";
-          mysql_query($sql);        
+  if($ok){
+    $dbAll=mysql_query("SELECT oname, mass FROM object where lname='".$lname."' ORDER BY oname");
+    $i=0;
+    $dbOname = array();
+    $dbMass = array();
+    while($row = mysql_fetch_array($dbAll)){
+      $dbOname[$i]=$row['oname'];
+      $dbMass[$i]=$row['mass'];
+      $i++;
+    }
+      
+      
+    for($i=0;$i<count($oname);$i++){
+      if($state[$i]!='normal'){
+        $exist=false;
+        for($j=0;$j<count($dbOname);$j++){
+          if($oname[$i]==$dbOname[$j]){
+            $exist=true;
+            if($state[$i]=='new'){
+              $sql="UPDATE object SET mass='".$mass[$i]."' WHERE lname='".$lname."' AND oname='".$oname[$i]."'";
+              mysql_query($sql);
+            }else if($state[$i]=='killed'){
+              $sql="DELETE from object where lname='".$lname."' AND oname='".$oname[$i]."'";
+              mysql_query($sql);        
+            }
+          }
+        }
+        if(!$exist){
+          if($state[$i]=='new'){
+            $sql="INSERT INTO object(`lname` ,`mass` ,`oname`)
+                  VALUES ('".$lname."', '".$mass[$i]."', '".$oname[$i]."')";
+            mysql_query($sql);  
+          }
         }
       }
     }
-    if(!$exist){
-      if($state[$i]=='new'){
-        $sql="INSERT INTO object(`lname` ,`mass` ,`oname`)
-              VALUES ('".$lname."', '".$mass[$i]."', '".$oname[$i]."')";
-        mysql_query($sql);  
-      }
+
+
+
+    $dbAll=mysql_query("SELECT oname, mass FROM object where lname='".$lname."' ORDER BY oname");
+    $i=0;
+    $dbOname = array();
+    $dbMass = array();
+    $dbstate = array();
+    $dbState[$i]="";
+    while($row = mysql_fetch_array($dbAll)){
+      $dbOname[$i]=$row['oname'];
+      $dbMass[$i]=$row['mass'];
+      $dbState[$i]='normal';
+      $i++;
     }
-  }
-}
-
-
-
-$dbAll=mysql_query("SELECT oname, mass FROM object where lname='".$lname."' ORDER BY oname");
-$i=0;
-$dbOname = array();
-$dbMass = array();
-$dbstate = array();
-$dbState[$i]="";
-while($row = mysql_fetch_array($dbAll)){
-  $dbOname[$i]=$row['oname'];
-  $dbMass[$i]=$row['mass'];
-  $dbState[$i]='normal';
-  $i++;
-}
 
 
 
 
-echo json_encode($dbOname);
-echo ";;;;;";
-echo json_encode($dbMass);
-echo ";;;;;";
-echo json_encode($dbState);
+    echo json_encode($dbOname);
+    echo ";;;;;";
+    echo json_encode($dbMass);
+    echo ";;;;;";
+    echo json_encode($dbState);
+  }else{
+    echo "0";
+  }//pw ok
 }else{
-echo "0";
-}
-//echo $oname[0];
-//echo "<br>".$strOname;
-
+  echo "0";
+}//lname!=0
