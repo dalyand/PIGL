9710feb9aa12e603efcd1a3a9bcdc7a83860816f
dalyand <dalyan@Darios-MacBook-Air.local>
2015-09-13 13:52:59 +0200

    Solved register problem

--- Source/register.php
+++ Source/register.php
@@ -20,6 +20,18 @@ $nrRow = mysql_num_rows(mysql_query("SELECT lname FROM list where lname='".$lnam
 if($nrRow==0){
     $dbAll=mysql_query("INSERT INTO list( lname, pw ) VALUES ('".$lname."', '".$pw."')");
     
+    $dbAll=mysql_query("SELECT lname, pw FROM list where lname='".$lname."'");
+    if($dbAll){
+    	$row = mysql_fetch_array($dbAll);
+    	if($row['pw']==$pw){
+      		echo $row['lname'];
+    	}else {
+     	 	echo "0";
+    	}
+    }else{
+    	echo "0";
+    }
+    flush();
     $to      = 'dario.duendar@gmail.com';
     $subject = 'New PIGL-List created';
     $message = "
@@ -31,13 +43,7 @@ if($nrRow==0){
         'X-Mailer: PHP/' . phpversion();
     mail($to, $subject, $message, $headers);
     
-    $dbAll=mysql_query("SELECT lname, pw FROM list where lname='".$lname."'");
-    $row = mysql_fetch_array($dbAll);
-    if($row['pw']==$pw){
-      echo $row['lname'];
-    }else {
-      echo "0";
-    }
 }else{
     echo "0";
 }
+
